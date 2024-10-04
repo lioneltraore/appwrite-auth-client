4562410cc050038bf1bfa60e1c5635f6308d490b
Lionel Traore <hello@lioneltraore.com>
2024-10-04 01:09:37 +0000

    refactor routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/auth/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/auth/register/register.page').then( m => m.RegisterPage)
   },
 ];
